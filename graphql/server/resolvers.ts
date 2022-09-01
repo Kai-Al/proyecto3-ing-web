1628a526f2308cc0083da309b5008037d99993b4
rainymarcelo <marcelo.delahozs@udea.edu.co>
2022-09-01 06:24:18 -0500

    Feat: types and resolvers for proyectos

--- graphql/server/resolvers.ts
+++ graphql/server/resolvers.ts
@@ -1,7 +1,12 @@
 import { Resolver } from 'types';
 import { model1Resolvers } from '@graphql/server/model1/resolvers';
 import { usuarioResolvers } from '@graphql/server/usuario/resolvers';
+import { proyectoResolvers } from '@graphql/server/proyecto/resolvers';
 
-const globalResolvers: Resolver[] = [model1Resolvers, usuarioResolvers];
+const globalResolvers: Resolver[] = [
+  model1Resolvers,
+  usuarioResolvers,
+  proyectoResolvers,
+];
 
 export { globalResolvers };
--- graphql/server/types.ts
+++ graphql/server/types.ts
@@ -2,11 +2,17 @@ import { DocumentNode } from 'graphql';
 import { gql } from 'apollo-server-micro';
 import { model1Types } from '@graphql/server/model1/types';
 import { usuarioTypes } from '@graphql/server/usuario/types';
+import { proyectoTypes } from '@graphql/server/proyecto/types';
 
 const CommonTypes = gql`
   scalar Date
 `;
 
-const globalTypes: DocumentNode[] = [CommonTypes, model1Types, usuarioTypes];
+const globalTypes: DocumentNode[] = [
+  CommonTypes,
+  model1Types,
+  usuarioTypes,
+  proyectoTypes,
+];
 
 export { globalTypes };
